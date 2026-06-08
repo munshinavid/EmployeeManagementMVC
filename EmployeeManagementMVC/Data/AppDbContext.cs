805517c8304e21d839642e89e4613ce98b015abb
munshinavid <navidmunshi24@gmail.com>
2026-06-08 18:01:33 +0600

    Initial MVC project setup

---
+++ EmployeeManagementMVC/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
